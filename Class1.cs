2a2b87b42e1b93ed9079b65c1a4c610a7775b946
marcaoftw <129304643+marcaoftw@users.noreply.github.com>
2023-04-07 12:04:07 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ExerciciosClasse
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+{
+        public string Nome;
+public int Idade;
+    }
+*/
--- Program.cs
+++ Program.cs
@@ -1,37 +1,58 @@
-﻿using System;
-using System.Globalization;
+﻿using ExerciciosClasse;
+using System;
+using System.Security.Cryptography;
 
-namespace Course
+namespace ExerciciosClasse
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite os valores de A, B e C: ");
-            string[] vet = Console.ReadLine().Split(' ');
-            double A = double.Parse(vet[0]);
-            double B = double.Parse(vet[1]);
-            double C = double.Parse(vet[2]);
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+            p1.Nome = "Carlos Silva";
+            p2.Nome = "Ana Marques";
+            p1.Salario = 6300;
+            p2.Salario = 6700;
 
-            double triangulo = (A * C)/2;
-            double circulo = (C * C) * 3.14159;
-            double trapezio = ((A + B) / 2) * C;
-            double quadrado = B * B;
-            double retangulo = A * B;
-
-            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
-            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3" , CultureInfo.InvariantCulture));
-            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture) );
-            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3" , CultureInfo.InvariantCulture));
-
-
-
-
-
+            Console.WriteLine("Dados da Primeira Pessoa:");
+            Console.WriteLine("Nome: " + p1.Nome);
+            Console.WriteLine("Idade: " + p1.Salario);
+            Console.WriteLine("Dados da Segunda Pessoa:");
+            Console.WriteLine("Nome: " + p2.Nome);
+            Console.WriteLine("Idade: " + p2.Salario);
+            Console.WriteLine("Salário médio: " + (p1.Salario + p2.Salario)/2);
         }
     }
-
 }
+
+
+
+/*
+{
+    Pessoa p1 = new Pessoa();
+    Pessoa p2 = new Pessoa();
+    p1.Nome = "Maria";
+    p2.Nome = "João";
+    p1.Idade = 17;
+    p2.Idade = 16;
+
+    Console.WriteLine("Dados da Primeira Pessoa:");
+    Console.WriteLine("Nome: " + p1.Nome);
+    Console.WriteLine("Idade: " + p1.Idade);
+    Console.WriteLine("Dados da Segunda Pessoa:");
+    Console.WriteLine("Nome: " + p2.Nome);
+    Console.WriteLine("Idade: " + p2.Idade);
+}
+*/
+
+
+
+
+
+
+
+
+
 
 
